b1baabd0f8c30237fe19197c7d0222458ca223aa
shemira <samuel871211@gmail.com>
2022-11-27 20:34:39 +0800

    feat: 新增HW2

---
+++ hw2/index.js
@@ -0,0 +1,65 @@
+const resultEl = document.getElementById('result');
+let resultStr = '';
+
+const button1 = document.getElementById('1');
+button1.addEventListener('click', handleClick1);
+function handleClick1 (mouseEvent) {
+  resultStr += '1';
+  resultEl.innerText = resultStr;
+};
+
+const button2 = document.getElementById('2');
+button2.addEventListener('click', handleClick2);
+function handleClick2 (mouseEvent) {
+  resultStr += '2';
+  resultEl.innerText = resultStr;
+};
+
+const button3 = document.getElementById('3');
+button3.addEventListener('click', handleClick3);
+function handleClick3 (mouseEvent) {
+  resultStr += '3';
+  resultEl.innerText = resultStr;
+};
+
+// const button1 = document.getElementById('1');
+// button1.addEventListener('click', handleClick1);
+// function handleClick1 (mouseEvent) {
+//   resultStr += '1';
+//   resultEl.innerText = resultStr;
+// };
+
+// const button1 = document.getElementById('1');
+// button1.addEventListener('click', handleClick1);
+// function handleClick1 (mouseEvent) {
+//   resultStr += '1';
+//   resultEl.innerText = resultStr;
+// };
+
+// const button1 = document.getElementById('1');
+// button1.addEventListener('click', handleClick1);
+// function handleClick1 (mouseEvent) {
+//   resultStr += '1';
+//   resultEl.innerText = resultStr;
+// };
+
+// const button1 = document.getElementById('1');
+// button1.addEventListener('click', handleClick1);
+// function handleClick1 (mouseEvent) {
+//   resultStr += '1';
+//   resultEl.innerText = resultStr;
+// };
+
+// const button1 = document.getElementById('1');
+// button1.addEventListener('click', handleClick1);
+// function handleClick1 (mouseEvent) {
+//   resultStr += '1';
+//   resultEl.innerText = resultStr;
+// };
+
+// const button1 = document.getElementById('1');
+// button1.addEventListener('click', handleClick1);
+// function handleClick1 (mouseEvent) {
+//   resultStr += '1';
+//   resultEl.innerText = resultStr;
+// };
